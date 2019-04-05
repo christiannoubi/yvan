3bf45a7202cdda56aff6fd53fd4b7729ad393fa1
Christian <noubichristian@yahoo.fr>
2019-04-05 13:57:54 +0200

    frist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserRowComponent } from './user-row/user-row.component';
+import { UserImageComponent } from './user-image/user-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UserListComponent,
+    UserRowComponent,
+    UserImageComponent
   ],
   imports: [
     BrowserModule
